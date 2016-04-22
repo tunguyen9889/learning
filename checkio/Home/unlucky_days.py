f27f79e91b517418c0c8a1df5cf1cee65a03aa49
Tu Nguyen <natu@lhv.vn>
2016-04-22 23:27:09 +0700

    Unlucky days

---
+++ checkio/Home/unlucky_days.py
@@ -0,0 +1,20 @@
+import calendar
+
+
+def checkio(year):
+    full_year = calendar.Calendar().yeardayscalendar(year, 12)
+    unlucky_days_count = 0
+    for month in range(0, 12):
+        for week in full_year[0][month]:
+            if week[4] == 13:
+                unlucky_days_count += 1
+    return unlucky_days_count
+
+# Clearance solution
+# def checkio(year):
+#     return sum(1 for i in range(1, 13) if calendar.weekday(year, i, 13) == 4)
+
+
+if __name__ == '__main__':
+    assert checkio(2015) == 3, "First - 2015"
+    assert checkio(1986) == 1, "Second - 1986"
