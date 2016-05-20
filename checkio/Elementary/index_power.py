ab7f600e22796330b6ea79562b5d55bd10b5955e
tunguyen9889 <natu@lhv.vn>
2016-05-20 16:30:22 +0700

    Index Power

---
+++ checkio/Elementary/index_power.py
@@ -0,0 +1,11 @@
+def index_power(array, n):
+    try:
+        return pow(array[n], n)
+    except IndexError:
+        return -1
+
+if __name__ == '__main__':
+    assert index_power([1, 2, 3, 4], 2) == 9, "Square"
+    assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
+    assert index_power([0, 1], 0) == 1, "Zero power"
+    assert index_power([1, 2], 3) == -1, "IndexError"
